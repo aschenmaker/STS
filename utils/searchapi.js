e6a47d7683ea13140d4e06db57498a8740363380
aschenmaker <aschen@cug.edu.cn>
2020-03-08 20:29:51 +0800

    修改 地址

--- utils/searchapi.js
+++ utils/searchapi.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://175.24.28.202:5000';
+const baseURL = 'http://www.jareee.com';
 const http = ({ url = '', params = {}, ...other } = {}) => {
 	wx.showLoading({
 		title: '加载中...'
