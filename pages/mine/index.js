62bec43f28608bebd4061009205aca3fcc3b139b
aschenmaker <aschen@cug.edu.cn>
2020-03-08 20:38:56 +0800

    读取用户信息

--- pages/mine/index.js
+++ pages/mine/index.js
@@ -1,66 +1,63 @@
 // pages/about/index.js
+var app = getApp();
 Page({
-
-  /**
+	/**
    * 页面的初始数据
    */
-  data: {
+	data: {
+		avatarUrl: '../../asset/img/avatar.png',
+		nickname: '未授权'
+	},
 
-  },
-
-  /**
+	/**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+	onLoad: function(options) {
+		// 设置用户头像，如果登录则取用户的用户头像
+		this.setUserPage();
+	},
 
-  },
+	setUserPage: function() {
+		var avatarUrl = app.globalData.userInfo.avatarUrl || avatarUrl;
+		var nickname = app.globalData.userInfo.nickName || nickname;
+		this.setData({
+			avatarUrl: avatarUrl,
+			nickname: nickname
+		});
+	},
 
-  /**
+	/**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+	onReady: function() {},
 
-  },
-
-  /**
+	/**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+	onShow: function() {},
 
-  },
-
-  /**
+	/**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+	onHide: function() {},
 
-  },
-
-  /**
+	/**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+	onUnload: function() {},
 
-  },
-
-  /**
+	/**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+	onPullDownRefresh: function() {},
 
-  },
-
-  /**
+	/**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+	onReachBottom: function() {},
 
-  },
-
-  /**
+	/**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+	onShareAppMessage: function() {}
+});
